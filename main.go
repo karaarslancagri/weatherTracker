338449715fd86ab8ff2bcce0daa7f5d7babdcd18
Çağrı Karaarslan <karaarslancagri1@gmail.com>
2024-01-10 16:40:40 +0300

    Finished

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 type apiConfigData struct {
@@ -32,6 +34,45 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 	return c, nil
 }
 
-func main() {
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from go!\n"))
+}
 
+func query(city string) (weatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiConfig.OpenWeatherMapApiKey + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/weather/",
+		func(w http.ResponseWriter, r *http.Request) {
+			city := strings.SplitN(r.URL.Path, "/", 3)[2]
+			data, err := query(city)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(data)
+		})
+
+	http.ListenAndServe(":8081", nil)
 }
