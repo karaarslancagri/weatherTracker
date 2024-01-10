4b27e682b63d2c87a300c9f3b6a020e7c411db7d
Çağrı Karaarslan <karaarslancagri1@gmail.com>
2024-01-10 16:15:15 +0300

    Created Config Methods

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type apiConfigData struct {
+	OpenWeatherMapApiKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadApiConfig(filename string) (apiConfigData, error) {
+	bytes, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return apiConfigData{}, err
+	}
+
+	var c apiConfigData
+
+	err = json.Unmarshal(bytes, &c)
+	if err != nil {
+		return apiConfigData{}, err
+	}
+	return c, nil
+}
+
 func main() {
 
 }
